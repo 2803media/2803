f6214678bccbd6baebeaa7282fea2dc400ed07dd
henri <hlabarre@gmail.com>
2011-07-18 09:55:24 +0200

    test

---
+++ synonymes.php
@@ -0,0 +1,22 @@
+<?php
+
+$synonymes = array ();
+$description = "";
+echo $description;
+echo '<p></p>';
+
+$sql="SELECT mot,synonymes  FROM synonymes"; 
+$result=mysql_query($sql) or die (mysql_error());
+while ($var = mysql_fetch_array($result) ) {
+	$synonymes[$var['mot']]=$var['synonymes'];
+}
+
+$output = strtr($description, $synonymes);
+echo $output;
+echo '<p></p>';
+$descriptionspin = spin($description, false);
+	
+echo $descriptionspin;
+echo '<hr>';
+
+?>
